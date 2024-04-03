f22b58641104d9e488c7e9162e6eb81fa94bf158
evencewu <evencewu@outlook.com>
2024-04-04 00:16:20 +0800

    update

--- omniisaacgymenvs/tasks/balance.py
+++ omniisaacgymenvs/tasks/balance.py
@@ -3,7 +3,7 @@ import math
 import numpy as np
 import torch
 from omni.isaac.core.articulations import ArticulationView
-from omni.isaac.core.utils.prims import get_prim_at_path
+#from omni.isaac.core.utils.prims import get_prim_at_pat
 from omniisaacgymenvs.tasks.base.rl_task import RLTask
 from omniisaacgymenvs.robots.articulations.balance import Balance
 
